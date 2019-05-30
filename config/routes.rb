d16ad1519794c76b4869fbb349857c09ec9d674a
mfjmori <mfjmori@gmail.com>
2019-05-30 14:20:50 +0900

    create profile/show.html.haml

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :users
+  resources :users do
+    resource :profiles only: :show
+  end
   resources :mypages
   resources :items
 end
