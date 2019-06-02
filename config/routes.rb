ed34421f1613d76fea36978cea6e84c39cc3d28e
iwama-takahiko <ae13013i@gmail.com>
2019-06-02 20:58:29 +0900

    devise_view_add

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,19 @@
 Rails.application.routes.draw do
 
   root 'items#index'
-  devise_for :users
-  resources :users, only: [:show, :edit] do
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions'
+   }
+  resources :users, only: [:show, :edit, :index] do
     resource :profiles, only: [:show, :edit]
     resource :addresses, only: [:edit]
+    collection do
+      get 'address'
+      get 'card'
+      get 'telephone'
+      get 'complete'
+    end
   end
   resources :cards, only: [:index, :new, :create]
   get "users/logout" => "users#logout"
@@ -12,3 +21,4 @@ Rails.application.routes.draw do
     resources :buy_orders, only: :new
   end
 end
+
