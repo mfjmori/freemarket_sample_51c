0d11f496951e3b93424bfce2a22ac0db70bc1eb0
mfjmori <mfjmori@gmail.com>
2019-06-05 20:25:04 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,17 +1,29 @@
 class ItemsController < ApplicationController
-    def index
-        
+  def index
+      
+  end
+  def new
+    @item = Item.new
+    10.times do 
+      @item.images.build
     end
-    def new
-    end
-    def create
-    end
-    def edit
-    end
-    def update
-    end
-    def destroy
-    end
-    def show
-    end
+  end
+  def create
+    item_parameter = item_params
+    @item = Item.new(item_parameter)
+    @item.save!
+  end
+  def edit
+  end
+  def update
+  end
+  def destroy
+  end
+  def show
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :description, :brand, :category_id, :size, :price, :postage, :shipping_method, :region, :shipping_date, :condition, images_attributes: [:image])
+  end
 end
