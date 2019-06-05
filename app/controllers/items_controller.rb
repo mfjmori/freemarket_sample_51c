70e2c86c4016d0ef450ed6c6f7c2b20b911c88ab
kenkengogo <open_sesami221@yahoo.co.jp>
2019-06-05 16:10:57 +0900

    surver.update1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,26 @@
 class ItemsController < ApplicationController
     def index
+        @categories_men = Category.find(1)
+        @items_men = @categories_men.items.order('id ASC').limit(4)
+
+        @categories_lady = Category.find(2)
+        @items_lady = @categories_lady.items.order('id ASC').limit(4)
+
+        @categories_baby = Category.find(3)
+        @items_baby = @categories_baby.items.order('id ASC').limit(4)
+
+        @categories_cosme = Category.find(4)
+        @items_cosme = @categories_cosme.items.order('id ASC').limit(4)
+
         
+        @items_Chanel = Item.where(brand: "シャネル").order('id ASC').limit(4)
+
+        @items_Louis_Vuitton = Item.where(brand: "ルイヴィトン").order('id ASC').limit(4)
+
+        @items_Supream= Item.where(brand: "シュプリーム").order('id ASC').limit(4)
+
+        @items_Nike= Item.where(brand: "ナイキ").order('id ASC').limit(4)
+
     end
     def new
     end
