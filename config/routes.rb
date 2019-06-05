da93d15400805bd90f150b6e88120f82c909d3ba
iwama-takahiko <ae13013i@gmail.com>
2019-06-05 18:27:16 +0900

    devise_login_logout add

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
 
   root 'items#index'
-  devise_for :users, :controllers => {
-    :registrations => 'users/registrations',
-    :sessions => 'users/sessions'
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
    }
-  resources :users, only: [:show, :edit, :index] do
+  resources :users, only: [:show, :edit, :new] do
     resource :profiles, only: [:show, :edit]
     resource :addresses, only: [:edit]
     collection do
@@ -13,12 +13,15 @@ Rails.application.routes.draw do
       get 'card'
       get 'telephone'
       get 'complete'
+      get 'logout'
     end
   end
+<<<<<<< Updated upstream
   resources :cards, only: [:index, :new, :create]
   get "users/logout" => "users#logout"
+=======
+>>>>>>> Stashed changes
   resources :items do
     resources :buy_orders, only: :new
   end
 end
-
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+<<<<<<< Updated upstream
 ActiveRecord::Schema.define(version: 2019_06_02_112003) do
+=======
+ActiveRecord::Schema.define(version: 2019_06_05_063201) do
+>>>>>>> Stashed changes
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "post_code", null: false
@@ -120,6 +124,7 @@ ActiveRecord::Schema.define(version: 2019_06_02_112003) do
     t.text "self_introduction"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "user_reviews", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
