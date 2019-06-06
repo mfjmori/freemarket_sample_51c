3b3ee554084694604c432b582e7299e0b2108c14
mfjmori <mfjmori@gmail.com>
2019-06-06 19:38:26 +0900

    fix association

--- app/models/item.rb
+++ app/models/item.rb
@@ -20,7 +20,7 @@ class Item < ApplicationRecord
   
     belongs_to :saler, class_name: "User"
     belongs_to :buyer, class_name: "User"
-    belongs_to :category
+    belongs_to :category, optional: true
     has_many :images, dependent: :destroy
     has_many :likes, dependent: :destroy
     has_many :comments, dependent: :destroy
