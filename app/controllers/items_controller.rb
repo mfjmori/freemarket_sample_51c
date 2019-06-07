413630723b8cb303ab30279ad0e89fe10f6fc0b6
mfjmori <mfjmori@gmail.com>
2019-06-07 16:14:19 +0900

    update item_params

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,11 @@
 class ItemsController < ApplicationController
+
+  before_action :move_to_sign_in, only: [:new]
+  layout 'user_application', only: :new
+
   def index
-      
   end
+
   def new
     @item = Item.new
     10.times do 
@@ -9,9 +13,9 @@ class ItemsController < ApplicationController
     end
     @parent_categories = Category.where(parent_id: 0)
   end
+
   def create
-    item_parameter = item_params
-    @item = Item.new(item_parameter)
+    @item = Item.new(item_params)
     @item.save!
   end
   def edit
@@ -24,7 +28,29 @@ class ItemsController < ApplicationController
   end
 
   private
+  def move_to_sign_in
+    redirect_to new_user_session_path unless user_signed_in?
+  end
+
   def item_params
-    params.require(:item).permit(:name, :description, :brand, :category_id, :size, :price, :postage, :shipping_method, :region, :shipping_date, :condition, images_attributes: [:image])
+    size_cloth = params.require(:item).permit(:size_cloth)[:size_cloth]
+    size_shoe = params.require(:item).permit(:size_shoe)[:size_shoe]
+    if size_cloth == "unanswered" && size_shoe == "unanswered"
+      size = "unanswered"
+    elsif size_cloth == "unanswered"
+      size = size_shoe
+    elsif size_shoe == "unanswered"
+      size = size_cloth
+    end
+    shipping_method_seller_cost = params.require(:item).permit(:shipping_method_seller_cost)[:shipping_method_seller_cost]
+    shipping_method_buyer_cost = params.require(:item).permit(:shipping_method_buyer_cost)[:shipping_method_buyer_cost]
+    if shipping_method_seller_cost == "" && shipping_method_buyer_cost == ""
+      shipping_method = ""
+    elsif shipping_method_seller_cost == ""
+      shipping_method = shipping_method_buyer_cost
+    elsif shipping_method_buyer_cost == ""
+      shipping_method = shipping_method_seller_cost
+    end
+    params.require(:item).permit(:name, :description, :brand, :category_id, :price, :postage, :region, :shipping_date, :condition, images_attributes: [:image]).merge(saler: current_user, size: size, shipping_method: shipping_method)
   end
 end
