5227a3f60195b0d182e52a719180ae66abdb8be7
mfjmori <mfjmori@gmail.com>
2019-06-07 09:59:00 +0900

    refactor item_new_preview.js

--- app/assets/javascripts/item_new_preview.js
+++ app/assets/javascripts/item_new_preview.js
@@ -2,13 +2,15 @@ $(function(){
   // ファイルが登録されているドロップゾーンの数を取得して、widthの値を返す
   function drop_zone_width() {
     // 全ドロップゾーンを取得
-    $all_drop_zone = $('.set-page__secound__form__text');
+    $all_drop_zone = $('.set-page__secound__form__text__drop');
     var file_full_number = 0;
     $all_drop_zone.each(function(i, drop_zone) {
+      // ファイルが登録されているドロップゾーンをカウント
       if ($(drop_zone).hasClass('file-full')) {
         file_full_number++;
       }
     });
+    // ファイル登録数によってwidthを返す
     switch(file_full_number) {
       case 0:
       case 5:
@@ -45,27 +47,24 @@ $(function(){
     // ドロップされたインデックスのドロップエリアを隠す
     $('.set-page__secound__form').hide();
     $(this).parent().hide();
-    $(this).parent().addClass('file-full');
+    $(this).addClass('file-full');
     // 全ドロップゾーンを取得
-    $all_drop_zone = $('.set-page__secound__form__text');
-    // ファイルが保存されていないドロップゾーンを１つだけ表示する
+    $all_drop_zone = $('.set-page__secound__form__text__drop');
+    // ファイルが保存されていないドロップゾーンを１つだけ表示する、widthも設定する
     $all_drop_zone.each(function(i, drop_zone) {
       if ($(drop_zone).hasClass('file-full') == false) {
-        $('.set-page__secound__form').show();
-        $(drop_zone).show();
-        // ドロップゾーンのwidthを設定
-        $(drop_zone).css('width', `${drop_zone_width()}px`);
-        $(drop_zone).parent().css('width', `${drop_zone_width()}px`);
-        $(drop_zone).children().css('width', `${drop_zone_width()}px`);
+        $('.set-page__secound__form').show().css('width', `${drop_zone_width()}px`);;
+        $(drop_zone).show().css('width', `${drop_zone_width()}px`);;
+        $(drop_zone).parent().show().css('width', `${drop_zone_width()}px`);;
         return false;
       }
     });
-    // ファイル読み込みが完了した際のイベント登録
+    // ファイル読み込みが完了した時
     reader.onload = (function(file) {
       return function(e) {
         //liとfigureと削除ボタンを追加
         $('.set-page__secound__form').before(`<div class="set-page__secound__preview__list" id="preview-${preview_index}"><figure class="set-page__secound__preview__list__figure"></figure><div class="set-page__secound__preview__list__delete-button" data-preview="${preview_index}">削除</div></div>`);
-        // .prevewの領域の中にロードした画像を表示するimageタグを追加
+        // .figureの中に画像を表示するimageタグを追加
         $preview = $(".set-page__secound__preview__list" + `#preview-${preview_index}`).children('figure');
         $preview.append($('<img>').attr({
                   src: e.target.result,
@@ -77,7 +76,6 @@ $(function(){
     })(file);
     reader.readAsDataURL(file);
   });
-
   // 削除ボタンが押されたとき
   $lists.on('click', '.set-page__secound__preview__list__delete-button' ,function() {
     // 削除ボタンのインデックスデータを取得する
@@ -87,12 +85,12 @@ $(function(){
     // 同じインデックスのフォームデータを空にする
     $('.set-page__secound__form__text__drop' + `#preview-${delete_preview_index}`).val("");
     // 同じインデックスのフォームのクラスを削除する
-    $('.set-page__secound__form__text__drop' + `#preview-${delete_preview_index}`).parent().removeClass('file-full');
+    $('.set-page__secound__form__text__drop' + `#preview-${delete_preview_index}`).removeClass('file-full');
     // 表示されているドロップエリアを隠す
     $(`.set-page__secound__form__text`).hide();
     // 同じインデックスのドロップエリアを表示する
     $('.set-page__secound__form').show().css('width', `${drop_zone_width()}px`);
-    $(`.set-page__secound__form__text--${delete_preview_index}`).show().css('width', `${drop_zone_width()}px`);
-    $(`.set-page__secound__form__text--${delete_preview_index}`).children().css('width', `${drop_zone_width()}px`);
+    $(`#preview-${delete_preview_index}`).parent().show().css('width', `${drop_zone_width()}px`);
+    $(`#preview-${delete_preview_index}`).css('width', `${drop_zone_width()}px`);
   })
 })
