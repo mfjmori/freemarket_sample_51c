231cff9e0e50afbaac83e601dee9c073b3f868de
mfjmori <mfjmori@gmail.com>
2019-06-07 16:10:54 +0900

    move view helper applicationhelper to item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,29 +1,42 @@
 class Item < ApplicationRecord
 
-enum size: {unanswered: 0, XXS: 1, XS: 2, S: 3, M: 4, L: 5, XL: 6, "2XL": 7, "3XL": 8, "4XL": 9, two_three: 10, two_three_five: 11, two_four: 12, two_four_five: 13, two_five: 14, two_five_five: 15, two_six: 16, two_six_five: 17, two_seven: 18, two_seven_five: 19, two_eight: 20, two_eight_five: 21, two_nine: 22, two_nine_five: 23, three_zero: 24, three_zero_five: 25, three_one: 26
-}
+  enum size: {unanswered: 0, XXS: 1, XS: 2, S: 3, M: 4, L: 5, XL: 6, "2XL": 7, "3XL": 8, "4XL": 9, two_three: 10, two_three_five: 11, two_four: 12, two_four_five: 13, two_five: 14, two_five_five: 15, two_six: 16, two_six_five: 17, two_seven: 18, two_seven_five: 19, two_eight: 20, two_eight_five: 21, two_nine: 22, two_nine_five: 23, three_zero: 24, three_zero_five: 25, three_one: 26}
+  enum condition: {new_item: 1, new_like: 2, no_dirt: 3, less_dirt: 4, dirt: 5, bad: 6}
+  enum postage: {seller_cost: 1, buyer_cost: 2}
+  enum shipping_method: {mitei: 1, rakuraku: 2, yuu: 3, letter: 4, normal: 5, cat: 6, pack: 7, post: 8, packet: 9}
+  enum region: {hokkaido: 0, aomori: 1, iwate: 2, miyagi: 3, akita: 4,yamagata: 5,fukushima: 6,ibaraki: 7,tochigi: 8,gunma: 9,saitama: 10,chiba: 11,tokyo: 12,kanagawa: 13,nigata: 14,toyama: 15,ishikawa: 16,fukui: 17,yamanashi: 18,nagano: 19,gifu: 20,sizuoka: 21,aichi: 22,mie: 23,shiga: 24,kyoto: 25,osaka: 26,hyogo: 27,nara: 28,wakayama: 29,tottori: 30,shimane: 31,okayama: 32,hiroshima: 33,yamaguchi: 34,tokushima: 35,kagawa: 36,ehime: 37,kochi: 38,fukuoka: 39,saga: 40,nagasaki: 41,kumamoto: 42,oita: 43,miyazaki: 44,kagoshima: 45,okinawa: 46}
+  enum shipping_date: {two: 1, three: 2, seven: 3}
 
-enum condition: {new_item: 1, new_like: 2, no_dirt: 3, less_dirt: 4, dirt: 5, bad: 6
-}
-
-enum postage: {seller_cost: 1, buyer_cost: 2
-}
-
-enum shipping_method: {mitei: 1, rakuraku: 2, yuu: 3, letter: 4, normal: 5, cat: 6, pack: 7, post: 8, packet: 9
-}
-
-enum region: {hokkaido: 0, aomori: 1, iwate: 2, miyagi: 3, akita: 4,yamagata: 5,fukushima: 6,ibaraki: 7,tochigi: 8,gunma: 9,saitama: 10,chiba: 11,tokyo: 12,kanagawa: 13,nigata: 14,toyama: 15,ishikawa: 16,fukui: 17,yamanashi: 18,nagano: 19,gifu: 20,sizuoka: 21,aichi: 22,mie: 23,shiga: 24,kyoto: 25,osaka: 26,hyogo: 27,nara: 28,wakayama: 29,tottori: 30,shimane: 31,okayama: 32,hiroshima: 33,yamaguchi: 34,tokushima: 35,kagawa: 36,ehime: 37,kochi: 38,fukuoka: 39,saga: 40,nagasaki: 41,kumamoto: 42,oita: 43,miyazaki: 44,kagoshima: 45,okinawa: 46
-}
-
-enum shipping_date: {two: 1, three: 2, seven: 3
-}
-
-  belongs_to :saler, class_name: "User", optional: true
+  belongs_to :saler, class_name: "User"
   belongs_to :buyer, class_name: "User", optional: true
-  belongs_to :category, optional: true
+  belongs_to :category
   has_many :images, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_one :buy_order
   accepts_nested_attributes_for :images
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :category, presence: true
+  validates :condition, presence: true
+  validates :saler, presence: true
+  validates :postage, presence: true
+  validates :shipping_method, presence: true
+  validates :region, presence: true
+  validates :shipping_date, presence: true
+  validates :price, presence: true, numericality: {only_integer: true, greater_than: 299, less_than: 10000000}
+  validates :images, presence: true
+
+  def self.set_coloth_size
+    self.sizes_i18n.slice(:unanswered, :XXS, :XS, :S, :M, :L, :XL, :"2XL", :"3XL", :"4XL").invert
+  end
+
+  def self.set_shoe_size
+    self.sizes_i18n.except(:XXS, :XS, :S, :M, :L, :XL, :"2XL", :"3XL", :"4XL").invert
+  end
+
+  def self.set_buyer_cost_shipping_method
+    self.shipping_methods_i18n.slice(:mitei, :yuu, :cat, :pack).invert
+  end
 end
