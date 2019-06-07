3cd9ee94046175e972f7c8e0ad00ead824c4fc99
iwama-takahiko <ae13013i@gmail.com>
2019-06-07 15:37:09 +0900

    devise strongparamater add

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth, if: :production?
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,profile_attributes: [:first_name, :last_name,:first_name_kana,:last_name_kana,:birthday]])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:nickname,profile_attributes: [:first_name, :last_name,:first_name_kana,:last_name_kana,:birthday]])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:nickname,profile_attributes: [:first_name, :last_name,:first_name_kana,:last_name_kana,:birthday]])
+  end
 
   private
   
