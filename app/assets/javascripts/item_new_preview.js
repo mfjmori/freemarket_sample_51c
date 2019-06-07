fe9169240a16808d9ad53468d878ce38e737ec78
mfjmori <mfjmori@gmail.com>
2019-06-07 10:26:18 +0900

    change to simple item_new_previews.js

--- app/assets/javascripts/item_new_preview.js
+++ app/assets/javascripts/item_new_preview.js
@@ -1,6 +1,6 @@
 $(function(){
-  // ファイルが登録されているドロップゾーンの数を取得して、widthの値を返す
-  function drop_zone_width() {
+  // ファイルが登録されているドロップゾーンの数を取得して、次に表示するドロップゾーンのwidthの値を返す
+  function get_drop_zone_width() {
     // 全ドロップゾーンを取得
     $all_drop_zone = $('.set-page__secound__form__text__drop');
     var file_full_number = 0;
@@ -53,9 +53,10 @@ $(function(){
     // ファイルが保存されていないドロップゾーンを１つだけ表示する、widthも設定する
     $all_drop_zone.each(function(i, drop_zone) {
       if ($(drop_zone).hasClass('file-full') == false) {
-        $('.set-page__secound__form').show().css('width', `${drop_zone_width()}px`);;
-        $(drop_zone).show().css('width', `${drop_zone_width()}px`);;
-        $(drop_zone).parent().show().css('width', `${drop_zone_width()}px`);;
+        var drop_zone_width = get_drop_zone_width();
+        $('.set-page__secound__form').show().css('width', `${drop_zone_width}px`);;
+        $(drop_zone).show().css('width', `${drop_zone_width}px`);;
+        $(drop_zone).parent().show().css('width', `${drop_zone_width}px`);;
         return false;
       }
     });
@@ -63,9 +64,9 @@ $(function(){
     reader.onload = (function(file) {
       return function(e) {
         //liとfigureと削除ボタンを追加
-        $('.set-page__secound__form').before(`<div class="set-page__secound__preview__list" id="preview-${preview_index}"><figure class="set-page__secound__preview__list__figure"></figure><div class="set-page__secound__preview__list__delete-button" data-preview="${preview_index}">削除</div></div>`);
+        $('.set-page__secound__form').before(`<div class="set-page__secound__preview__list" id="preview-img-${preview_index}"><figure class="set-page__secound__preview__list__figure"></figure><div class="set-page__secound__preview__list__delete-button" data-preview="${preview_index}">削除</div></div>`);
         // .figureの中に画像を表示するimageタグを追加
-        $preview = $(".set-page__secound__preview__list" + `#preview-${preview_index}`).children('figure');
+        $preview = $(".set-page__secound__preview__list" + `#preview-img-${preview_index}`).children('figure');
         $preview.append($('<img>').attr({
                   src: e.target.result,
                   width: "150px",
@@ -81,16 +82,17 @@ $(function(){
     // 削除ボタンのインデックスデータを取得する
     var delete_preview_index = $(this).data('preview');
     // 同じインデックスの画像プレビューを消す
-    $('.set-page__secound__preview__list' + `#preview-${delete_preview_index}`).remove();
+    $(`#preview-img-${delete_preview_index}`).remove();
     // 同じインデックスのフォームデータを空にする
-    $('.set-page__secound__form__text__drop' + `#preview-${delete_preview_index}`).val("");
+    $(`#preview-${delete_preview_index}`).val("");
     // 同じインデックスのフォームのクラスを削除する
-    $('.set-page__secound__form__text__drop' + `#preview-${delete_preview_index}`).removeClass('file-full');
+    $(`#preview-${delete_preview_index}`).removeClass('file-full');
     // 表示されているドロップエリアを隠す
     $(`.set-page__secound__form__text`).hide();
-    // 同じインデックスのドロップエリアを表示する
-    $('.set-page__secound__form').show().css('width', `${drop_zone_width()}px`);
-    $(`#preview-${delete_preview_index}`).parent().show().css('width', `${drop_zone_width()}px`);
-    $(`#preview-${delete_preview_index}`).css('width', `${drop_zone_width()}px`);
+    // 同じインデックスのドロップエリアを表示する,widthも設定する
+    var drop_zone_width = get_drop_zone_width();
+    $('.set-page__secound__form').show().css('width', `${drop_zone_width}px`);
+    $(`#preview-${delete_preview_index}`).parent().show().css('width', `${drop_zone_width}px`);
+    $(`#preview-${delete_preview_index}`).css('width', `${drop_zone_width}px`);
   })
 })
