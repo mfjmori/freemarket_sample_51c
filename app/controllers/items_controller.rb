a5dc6a0587ab7a3130e4aa6b7e06cc9af62fed1b
asai569 <sumo1231mo@gmail.com>
2019-06-08 11:27:39 +0900

    sume

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,17 +1,34 @@
 class ItemsController < ApplicationController
     def index
-
+        @items = Item.all
     end
     def new
     end
-    # def create
-    # end
+    def create
+    end
     def edit
     end
     def update
     end
     def destroy
     end
+
     def show
+        @items = Item.find(params[:id])
+        @users = User.find(params[:id])
+
+
+        @theparent =Category.find(params[:id])
+        category = Item.find(params[:id])
+        groundchild_id =category.category_id
+        @groundchild =Category.find_by(id: groundchild_id)
+        child_id =@groundchild.parent_id
+        @child=Category.find_by(id: child_id)
     end
+
+    private
+    def item_params
+        params.permit(:image,:text)
+    end
+
 end
