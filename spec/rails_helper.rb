2e6c662ceb09fc67b28339c3fa4e26ed4968ceaf
asai569 <sumo1231mo@gmail.com>
2019-06-09 13:36:45 +0900

    Update rails_helper.rb

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -19,13 +19,7 @@ require 'rspec/rails'
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-<<<<<<< HEAD
 # Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
-=======
-
-Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
-
->>>>>>> 0b7309c1d5824f7889f3f395df847b9004c35510
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
 begin
@@ -35,18 +29,8 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 RSpec.configure do |config|
-<<<<<<< HEAD
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
-  config.include Devise::Test::ControllerHelpers, type: :controller
-  config.include FactoryBot::Syntax::Methods
-=======
-
-
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
->>>>>>> 0b7309c1d5824f7889f3f395df847b9004c35510
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
