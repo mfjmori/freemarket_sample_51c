1de01eabc7d90573534e8e4f21f14f5c02ddbe75
asai569 <sumo1231mo@gmail.com>
2019-06-09 13:19:29 +0900

    Update items.rb

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,9 +1,17 @@
 
 FactoryBot.define do
+
   factory :item do
-    name {"nana"}
-    id {1}
-    saler_id {1}
-    buyer_id {2}
+    name              {Faker::Name.name}
+    description       {Faker::Lorem.sentence}
+    category_id       {Category.new(id: 1)}
+    condition         {"new_like"}
+    saler             {User.new(id: 1)}
+    buyer             {User.new(id: 1)}
+    postage           {"seller_cost"}
+    shipping_method   {"rakuraku"}
+    region            {"hokkaido"}
+    shipping_date     {"two"}
+    price             {1000}
   end
 end
