0f63d950d87c90fea80999680e49645bd7bc37e5
asai569 <sumo1231mo@gmail.com>
2019-06-09 12:35:15 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
+
 class ItemsController < ApplicationController
     def index
-        
+        @items = Item.all
     end
     def new
     end
@@ -12,6 +13,24 @@ class ItemsController < ApplicationController
     end
     def destroy
     end
+
     def show
+        @items = Item.find(params[:id])
+        @users = @items.saler
+
+
+        @theparent =Category.find(params[:id])
+        category = Item.find(params[:id])
+        groundchild_id =category.category_id
+        @groundchild =Category.find_by(id: groundchild_id)
+        child_id =@groundchild.parent_id
+        @child=Category.find_by(id: child_id)
     end
+
+    private
+    def item_params
+        params.permit(:image,:text)
+    end
+
 end
+
