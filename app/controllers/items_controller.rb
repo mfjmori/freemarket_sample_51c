dec89c7238f9fdd22dd0aa5c0e1dbf0253490e1f
kenkengogo <open_sesami221@yahoo.co.jp>
2019-06-10 21:37:23 +0900

    item.index.surver.upgrade10

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,20 +7,28 @@ class ItemsController < ApplicationController
     def index
     
       @ladies_child_category = Category.where(parent_id:1)
-      @ladies_grand_child_category = Category.where(parent_id: @ladies_child_category.each do |child| child.id end)
-      @items_lady =  Item.where(category_id:@ladies_grand_child_category.each do |grand_child| grand_child.id end).order('id ASC').limit(4)
+      ladies_child_ids = @ladies_child_category.map{|child| child.id}
+      @ladies_grand_child_category = Category.where(parent_id: ladies_child_ids)
+      ladies_grand_child_ids = @ladies_grand_child_category.map{|grand_child| grand_child.id}
+      @items_lady =  Item.where(category_id:ladies_grand_child_ids).order('id ASC').limit(4)
 
       @mens_child_category = Category.where(parent_id:2)
-      @mens_grand_child_category = Category.where(parent_id: @mens_child_category.each do |child| child.id end)
-      @items_men =  Item.where(category_id:@mens_grand_child_category.each do |grand_child| grand_child.id end).order('id ASC').limit(4)
+      mens_child_ids = @mens_child_category.map{|child| child.id}
+      @mens_grand_child_category = Category.where(parent_id: mens_child_ids)
+      mens_grand_child_ids = @mens_grand_child_category.map{|grand_child| grand_child.id}
+      @items_men =  Item.where(category_id:mens_grand_child_ids).order('id ASC').limit(4)
       
       @baby_child_category = Category.where(parent_id:3)
-      @baby_grand_child_category = Category.where(parent_id: @baby_child_category.each do |child| child.id end)
-      @items_baby =  Item.where(category_id:@baby_grand_child_category.each do |grand_child| grand_child.id end).order('id ASC').limit(4)
+      baby_child_ids = @baby_child_category.map{|child| child.id}
+      @baby_grand_child_category = Category.where(parent_id: baby_child_ids)
+      baby_grand__child_ids = @baby_grand_child_category.map{|grand_child| grand_child.id}
+      @items_baby =  Item.where(category_id:baby_grand__child_ids).order('id ASC').limit(4)
 
       @cosmes_child_category = Category.where(parent_id:7)
-      @cosmes_grand_child_category = Category.where(parent_id: @cosmes_child_category.each do |child| child.id end)
-      @items_cosme =  Item.where(category_id:@cosmes_grand_child_category.each do |grand_child| grand_child.id end).order('id ASC').limit(4)
+      cosmes_child_ids = @cosmes_child_category.map{|child| child.id}
+      @cosmes_grand_child_category = Category.where(parent_id: cosmes_child_ids)
+      cosmes_grand__child_ids = @cosmes_grand_child_category.map{|grand_child| grand_child.id}
+      @items_cosme =  Item.where(category_id:cosmes_grand__child_ids).order('id ASC').limit(4)
 
 
       @items_Chanel = Item.where(brand: "シャネル").order('id ASC').limit(4)
