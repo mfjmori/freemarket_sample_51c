0a50430b091bf4fc810c258f6b6d834b0ea81d61
seiya <setty.824@docomo.ne.jp>
2019-06-12 15:58:32 +0900

    Concerns

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,34 +1,20 @@
 class ItemsController < ApplicationController
+
+  include CommonActions
   before_action :set_item, only: [:show, :destroy, :edit, :update, :stop]
     before_action :move_to_sign_in, only: [:new, :edit, :destroy, :stop]
   layout 'item_application', only: [:new, :edit]
   
     def index
       @items = Item.all.where.not(status: true)
-
-      @ladies_child_category = Category.where(parent_id:1)
-      ladies_child_ids = @ladies_child_category.map{|child| child.id}
-      @ladies_grand_child_category = Category.child_num(ladies_child_ids)
-      ladies_grand_child_ids = @ladies_grand_child_category.map{|grand_child| grand_child.id}
-      @items_lady =  @items.item_num(ladies_grand_child_ids)
-
-      @mens_child_category = Category.where(parent_id:2)
-      mens_child_ids = @mens_child_category.map{|child| child.id}
-      @mens_grand_child_category = Category.child_num(mens_child_ids)
-      mens_grand_child_ids = @mens_grand_child_category.map{|grand_child| grand_child.id}
-      @items_men =  @items.item_num(mens_grand_child_ids)
       
-      @baby_child_category = Category.where(parent_id:3)
-      baby_child_ids = @baby_child_category.map{|child| child.id}
-      @baby_grand_child_category = Category.child_num(baby_child_ids)
-      baby_grand__child_ids = @baby_grand_child_category.map{|grand_child| grand_child.id}
-      @items_baby =  @items.item_num(baby_grand__child_ids)
-
-      @cosmes_child_category = Category.where(parent_id:7)
-      cosmes_child_ids = @cosmes_child_category.map{|child| child.id}
-      @cosmes_grand_child_category = Category.child_num(cosmes_child_ids)
-      cosmes_grand__child_ids = @cosmes_grand_child_category.map{|grand_child| grand_child.id}
-      @items_cosme =  @items.item_num(cosmes_grand__child_ids)
+      @items_lady =  @items.item_num(ladies_category)
+      
+      @items_men =  @items.item_num(mens_category)
+      
+      @items_baby =  @items.item_num(baby_category)
+      
+      @items_cosme =  @items.item_num(cosmes_category)
 
       @items_Chanel = @items.item_brand("シャネル")
 
@@ -132,5 +118,7 @@ class ItemsController < ApplicationController
   def set_item
     @item = Item.find(params[:id])
   end
+
+  
 end
 
