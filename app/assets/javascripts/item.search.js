e35ad3afddb0212a8cf867aae000931a362b0ce1
mfjmori <mfjmori@gmail.com>
2019-06-15 20:22:25 +0900

    add price search auto input

--- app/assets/javascripts/item.search.js
+++ app/assets/javascripts/item.search.js
@@ -75,4 +75,12 @@ $(function() {
       alert('error');
     })
   })
+  // 価格の範囲が選択された時、MINとMAXを入力する
+  $('.search-form__detail__price__pull-down__select').change(function() {
+    var price_range = $(this).children('option:selected').val().split('~', 2);
+    var min_price = price_range[0];
+    var max_price = price_range[1];
+    $('.search-form__detail__price__inputs__min').val(min_price);
+    $('.search-form__detail__price__inputs__max').val(max_price);
+  })
 })
