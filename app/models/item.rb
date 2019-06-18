369e707fa60d3368898f1b286935bb4641b53e01
mfjmori <mfjmori@gmail.com>
2019-06-18 22:08:27 +0900

    add likes sort

--- app/models/item.rb
+++ app/models/item.rb
@@ -54,4 +54,9 @@ class Item < ApplicationRecord
   scope :recent, -> {order('id ASC')}
   scope :recent, -> {order('updated_at DESC')}
   scope :on_sale, -> {where(status: 0)}
+
+  ransacker :likes_count do
+    query = '(SELECT COUNT(likes.item_id) FROM likes where likes.item_id = items.id GROUP BY likes.item_id)'
+    Arel.sql(query)
+  end
 end
