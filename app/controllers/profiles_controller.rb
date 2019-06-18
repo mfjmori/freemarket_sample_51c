6e22083114147f8b117e05065dd30e6a97862827
mfjmori <mfjmori@gmail.com>
2019-06-18 19:06:01 +0900

    fix profile show

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,7 +2,7 @@ class ProfilesController < ApplicationController
 
   before_action :set_card
   def show
-    @users = User.find_by(params[:id])
+    @users = User.find(params[:user_id])
   end
   def edit
     @users = User.find_by(params[:id])
